87a497d34270a81fa98a4bd3a6c3f894d756e454
Ryazan <ryazangaming@gmail.com>
2019-06-25 11:14:08 +0500

    HW3 base howework 1. Repair InMemory...ControllerTest and SpringTest. 2. Repair SpringMain 3. Add init table 'meals' in PostgreSQL DB with field in Meal Entity and user id foreign key 4. Add populateDB data meals for user and admin user 5. Realized JdbcMealRepositoryImpl CRUD operations (+ getAll with filter) in database with Meal entity

--- src/test/java/ru/javawebinar/topjava/web/user/InMemoryAdminRestControllerSpringTest.java
+++ src/test/java/ru/javawebinar/topjava/web/user/InMemoryAdminRestControllerSpringTest.java
@@ -12,11 +12,12 @@ import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.inmemory.InMemoryUserRepository;
 import ru.javawebinar.topjava.util.exception.NotFoundException;
 
+import javax.annotation.Resource;
 import java.util.Collection;
 
 import static ru.javawebinar.topjava.UserTestData.ADMIN;
 
-@ContextConfiguration("classpath:spring/spring-app.xml")
+@ContextConfiguration("classpath:spring/spring-app-inmemory.xml")
 @RunWith(SpringRunner.class)
 public class InMemoryAdminRestControllerSpringTest {
 
