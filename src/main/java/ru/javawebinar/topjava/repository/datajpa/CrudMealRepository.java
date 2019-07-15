1d8cf9ceb67bc6fad308dfaea25c26491d754c2e
Ryazan <ryazangaming@gmail.com>
2019-07-15 15:33:12 +0500

    HW5 homework: DataJpaMealRepository

--- src/main/java/ru/javawebinar/topjava/repository/datajpa/CrudMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/datajpa/CrudMealRepository.java
@@ -1,7 +1,24 @@
 package ru.javawebinar.topjava.repository.datajpa;
 
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.model.User;
+
+import javax.transaction.Transactional;
+import java.time.LocalDateTime;
+import java.util.List;
 
 public interface CrudMealRepository extends JpaRepository<Meal, Integer> {
+
+    @Transactional
+    @Modifying
+    @Query("DELETE FROM Meal m WHERE m.id=:id and m.user.id=:userId")
+    int delete(@Param("id") int id, @Param("userId") int userId);
+
+    List<Meal> findAllByDateTimeBetweenAndUser(LocalDateTime startDateTime, LocalDateTime endDateTime, User user, Sort sort);
+    List<Meal> findAllByUser(User user, Sort sort);
 }
--- src/main/java/ru/javawebinar/topjava/web/MealServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/MealServlet.java
@@ -29,7 +29,10 @@ public class MealServlet extends HttpServlet {
     @Override
     public void init(ServletConfig config) throws ServletException {
         super.init(config);
-        springContext = new ClassPathXmlApplicationContext("spring/spring-app.xml", "spring/spring-db.xml");
+        ClassPathXmlApplicationContext springContext = new ClassPathXmlApplicationContext();
+        springContext.getEnvironment().setActiveProfiles("postgres", "datajpa");
+        springContext.setConfigLocations("spring/spring-app.xml", "spring/spring-db.xml");
+        springContext.refresh();
         mealController = springContext.getBean(MealRestController.class);
     }
 
